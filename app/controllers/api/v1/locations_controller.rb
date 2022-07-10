a4de73e0d7a0352a539da95377345a7ee067e0d8
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2022-07-10 18:11:42 -0300

    api controller and json builder

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,13 +1,12 @@
-class Api::V1::LocationsController < ApplicationController
+class Api::V1::LocationsController < ApiController
     before_action :set_location
 
     def show
-        
     end
     
     private
 
-    def ser_location
+    def set_location
         @location = Location.find(params[:id])
     end
     
