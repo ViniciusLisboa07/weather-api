1e09387956d124aa59875777423c0deda0a795f7
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2022-07-10 22:58:14 -0300

    jwt configs to create user

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,5 @@
 class ApiController < ApplicationController
+    protect_from_forgery with: :null_session
     before_action :set_default_format
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :users, only: [:create]
+      post "/login", to: "users#login"
+      get "/auto_login", to: "users#auto_login"
+      
       resources :locations do
         resources :recordings
       end
